f58a4f269dd509210b784c839b0a844ba5dcc476
Andreas Karis <ak.karis@gmail.com>
2023-03-14 12:11:50 +0100

    reverting dropwatch
    Signed-off-by: Andreas Karis <ak.karis@gmail.com>

--- resources/collect-metrics.sh
+++ resources/collect-metrics.sh
@@ -9,22 +9,6 @@ DATA_STORE=${DATA_STORE:-"/data-store/network-metrics"}
 NOW=$(date +%Y_%m_%d_%H)
 DATA_STORE_NETWORK_METRICS="${HOSTNAME}-network_metrics_${NOW}"
 
-# record_packet_drops uses perf to record packet drops. Sourced from https://access.redhat.com/solutions/5859751.
-# record_packet_drops() {
-#   local iterations="${1}"
-#   local interval="${2}"
-#   mkdir -p "${DATA_STORE_NETWORK_METRICS}/dropwatch"
-#   local c=0
-#   while [ "$c" -lt "${iterations}" ]; do
-#     date >> "${DATA_STORE_NETWORK_METRICS}/dropwatch/dropwatch-${c}.txt"
-#     (echo "stop"; echo "start"; echo "stop"; echo "exit") | dropwatch >> "${DATA_STORE_NETWORK_METRICS}/dropwatch/dropwatch-${c}.txt" &
-#     local pid=$!
-#     sleep "${interval}"
-#     kill ${pid}
-#     c=$((c + 1))
-#   done
-# }
-
 echo "Gathering metrics ..."
 
 mkdir -p "${DATA_STORE_NETWORK_METRICS}"
@@ -48,7 +32,6 @@ CONNTRACK=$!
 bash -c "while true ; do date ; ip -s -s link ls ; sleep ${INTERVAL}; done" > "${DATA_STORE_NETWORK_METRICS}/ip_link.txt" &
 IP_LINK=$!
 echo "Metrics gathering started. Please wait for completion..."
-# record_packet_drops "${ITERATIONS}" "${INTERVAL}"
 sleep "${DURATION}"
 kill $PIDSTAT
 kill $SAR
